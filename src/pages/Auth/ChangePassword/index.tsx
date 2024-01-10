bb26912e3ac7f47281c9029f4e6ddb3e7893b74a
Saulomelo <saulo.melo@edu.unifor.br>
2024-01-10 20:11:41 -0300

    ✨ Hook form nas telas de registro e Mudar senha

--- src/pages/Auth/ChangePassword/index.tsx
+++ src/pages/Auth/ChangePassword/index.tsx
@@ -1,55 +1,70 @@
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import img from '../../../assets/icon.svg';
-import { getI18n } from '../../../utils/hooks/useGetI18n';
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { classNames } from "primereact/utils";
+
+import { getI18n } from "../../../utils/hooks/useGetI18n";
+import { LogoTopbar } from "../../../components/LogoTopbar";
+import { getFormErrorMessage } from "../../../utils/hooks/useGetFormErrorMessage";
+import { useEffect } from "react";
 
 export const ChangePassowrdPage = () => {
-	const changePasswordI18n = getI18n('changePassword');
-	const [value, setValue] = useState('');
-	const navigate = useNavigate();
+  const changePasswordI18n = getI18n("changePassword");
+  const navigate = useNavigate();
+  const {
+    formState: { errors },
+    handleSubmit,
+    watch,
+    register,
+  } = useForm();
 
-	return (
-		<div>
-			<div
-				className="absolute"
-				style={{ top: 20, left: 20 }}
-				onClick={() => {
-					navigate('/');
-				}}
-			>
-				<img src={img} alt="" style={{ borderRadius: 5, overflow: 'hidden' }} />
-			</div>
-			<div className="h-screen w-screen flex justify-content-center align-items-center">
-				<div className="flex flex-column w-16rem">
-					<div className="text-center mb-3">
-						<h1 className="m-0">{changePasswordI18n.title}</h1>
-						<span>{changePasswordI18n.description}</span>
-					</div>
-					<div className="mb-3">
-						<InputText
-							className="w-full"
-							value={value}
-							placeholder={changePasswordI18n.email}
-							onChange={(e) => setValue(e.target.value)}
-						/>
-					</div>
-					<div className="mb-3">
-						<Button className="w-full" label={changePasswordI18n.send} />
-					</div>
-					<div className="text-center">
-						<span
-							onClick={() => {
-								navigate('/');
-							}}
-							className="no-underline hover:underline text-primary cursor-pointer ml-2"
-						>
-							{changePasswordI18n.back}
-						</span>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    watch("value");
+  }, [watch("value")]);
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <LogoTopbar />
+      <div className="h-screen w-screen flex justify-content-center align-items-center">
+        <div className="flex flex-column w-16rem">
+          <div className="text-center mb-3">
+            <h1 className="mb-3">{changePasswordI18n.title}</h1>
+            <span>{changePasswordI18n.description}</span>
+          </div>
+          <div className="mb-3">
+            <InputText
+              className={classNames({
+                "p-invalid": errors.email,
+              })}
+              style={{ width: "100%" }}
+              placeholder={changePasswordI18n.email}
+              id="login"
+              {...register("email", {
+                required: true,
+              })}
+            />
+            {getFormErrorMessage(errors.email)}
+          </div>
+          <div className="mb-3">
+            <Button className="w-full" label={changePasswordI18n.send} />
+          </div>
+          <div className="text-center">
+            <span
+              onClick={() => {
+                navigate("/");
+              }}
+              className="no-underline hover:underline text-primary cursor-pointer ml-2"
+            >
+              {changePasswordI18n.back}
+            </span>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 };
--- src/pages/Auth/Register/index.tsx
+++ src/pages/Auth/Register/index.tsx
@@ -1,70 +1,95 @@
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { Password } from 'primereact/password';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import img from '../../../assets/icon.svg';
-import { getI18n } from '../../../utils/hooks/useGetI18n';
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { Password } from "primereact/password";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { classNames } from "primereact/utils";
+
+import { getI18n } from "../../../utils/hooks/useGetI18n";
+import { LogoTopbar } from "../../../components/LogoTopbar";
+import { getFormErrorMessage } from "../../../utils/hooks/useGetFormErrorMessage";
 
 export const RegisterPage = () => {
-	const registerI18n = getI18n('register');
-	const [value, setValue] = useState('');
-	const [password, setPassword] = useState('');
-	const navigate = useNavigate();
+  const registerI18n = getI18n("register");
+  const navigate = useNavigate();
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    watch,
+    register,
+  } = useForm();
 
-	//TODO: Trocar o import para um atalho no tsconfig.json
-	return (
-		<div>
-			<div
-				className="absolute cursor-pointer"
-				style={{ top: 20, left: 20 }}
-				onClick={() => {
-					navigate('/');
-				}}
-			>
-				<img src={img} alt="" style={{ borderRadius: 5, overflow: 'hidden' }} />
-			</div>
-			<div className="h-screen w-screen flex justify-content-center align-items-center">
-				<div className="flex flex-column w-16rem">
-					<div className="text-center">
-						<h1>{registerI18n.title}</h1>
-					</div>
-					<div className="mb-3">
-						<InputText
-							className="w-full"
-							value={value}
-							placeholder={registerI18n.login}
-							onChange={(e) => setValue(e.target.value)}
-						/>
-					</div>
-					<div className="mb-3">
-						<Password
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-							placeholder={registerI18n.password}
-							toggleMask
-							inputStyle={{ width: '100%' }}
-							feedback={false}
-						/>
-					</div>
-					<div className="mb-3">
-						<Button className="w-full" label={registerI18n.create_account} />
-					</div>
-					<div className="text-center">
-						<div>
-							<span>{registerI18n.already_have_an_account}</span>
-							<span
-								onClick={() => {
-									navigate('/');
-								}}
-								className="no-underline hover:underline text-primary cursor-pointer ml-2"
-							>
-								{registerI18n.go_login}
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    watch("value");
+  }, [watch("value")]);
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  //TODO: Trocar o import para um atalho no tsconfig.json
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <LogoTopbar />
+      <div className="h-screen w-screen flex justify-content-center align-items-center">
+        <div className="flex flex-column w-16rem">
+          <div className="text-center mb-2">
+            <h1>{registerI18n.title}</h1>
+          </div>
+          <div className="mb-2">
+            <InputText
+              className={classNames({
+                "p-invalid": errors.login,
+              })}
+              style={{ width: "100%" }}
+              placeholder={registerI18n.login}
+              id="login"
+              {...register("login", {
+                required: true,
+              })}
+            />
+            {getFormErrorMessage(errors.login)}
+          </div>
+          <div className="mb-3">
+            <Controller
+              name="password"
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
+                <>
+                  <Password
+                    onChange={(e) => field.onChange(e)}
+                    placeholder={registerI18n.password}
+                    className={classNames({ "p-invalid": fieldState.error })}
+                    feedback={false}
+                    toggleMask
+                    inputStyle={{ width: "100%" }}
+                  />
+                  {getFormErrorMessage(errors.password)}
+                </>
+              )}
+            />
+          </div>
+          <div className="mb-3">
+            <Button className="w-full" label={registerI18n.create_account} />
+          </div>
+          <div className="text-center">
+            <div>
+              <span>{registerI18n.already_have_an_account}</span>
+              <span
+                onClick={() => {
+                  navigate("/");
+                }}
+                className="no-underline hover:underline text-primary cursor-pointer ml-2"
+              >
+                {registerI18n.go_login}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 };

a118c8cf2337927935588705c4828064030e058c
Saulomelo <saulo.melo@edu.unifor.br>
2024-01-12 11:43:14 -0300

    🐛 Scroll funcionando

--- src/pages/Auth/Home/index.tsx
+++ src/pages/Auth/Home/index.tsx
@@ -8,8 +8,8 @@ export const HomePage = () => {
   const loginI18n = getI18n("login");
 
   return (
-    <div className="h-screen w-screen flex flex-column">
-      <header className="flex w-screen h-6rem align-items-center justify-content-between">
+    <div className="">
+      <header className="flex h-6rem align-items-center justify-content-between">
         <div className="flex">
           <div className="flex align-items-center">
             <img src={img} style={{ borderRadius: 5 }} className="mx-4" />
@@ -30,49 +30,48 @@ export const HomePage = () => {
         </div>
       </header>
       <main
-        className="flex"
+        className="flex flex-column"
         style={{
-          //height: "calc(100vh - 6rem)",
+          height: "calc(200vh - 6rem)",
           overflowY: "auto",
         }}
       >
         <div
           className="flex"
-          style={{ backgroundColor: "#88A9C3", overflowY: "auto" }}
+          style={{
+            height: "calc(100vh - 6rem)",
+            backgroundColor: "#88A9C3",
+            overflowY: "auto",
+          }}
         >
           <div className="flex w-screen align-items-center justify-content-evenly">
-            <div>
+            <div className="mx-4">
               <div>
                 <h1>
                   Sua ferramenta para <br /> a busca de advogados
                 </h1>
               </div>
               <div>
-                <p>
+                <span>
                   Procurando um Advogado? <br /> O Advogapp te ajuda a encontrar
                   o advogado para o seu caso
-                </p>
+                </span>
               </div>
             </div>
-            <div>
-              <img
-                src={img_men}
-                style={{
-                  borderRadius: 5,
-                  boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
-                }}
-                className="mx-4"
-              />
+            <div className="mx-8">
+              <img src={img_men} style={{ borderRadius: 5 }} className="mx-4" />
             </div>
           </div>
         </div>
         <div
-          className="flex"
+          className="flex m-8"
           style={{
             height: "100vh",
             overflowY: "auto",
+            backgroundColor: "#ffffff",
           }}
         >
+          <span>aaa</span>
           {/* <Dropdown
           value={selectedCountry}
           onChange={(e) => setSelectedCountry(e.value)}

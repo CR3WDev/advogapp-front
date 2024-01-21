2fb25eb68b5bf9e71a2be6536b83ddf9f9b9ea1d
Saulomelo <saulo.melo@edu.unifor.br>
2024-01-21 14:55:53 -0300

    ✨ i18n para a tela home

--- src/pages/Auth/Home/index.tsx
+++ src/pages/Auth/Home/index.tsx
@@ -1,92 +1,92 @@
-import { Button } from 'primereact/button';
-import { Divider } from 'primereact/divider';
-import { useNavigate } from 'react-router-dom';
-import img_men from '../../../assets/home_imj.jpg';
-import img from '../../../assets/icon.svg';
-import { CardAdv } from '../../../components/adv/CardAdvogados';
+import { Button } from "primereact/button";
+import { Divider } from "primereact/divider";
+import { useNavigate } from "react-router-dom";
+import img_men from "../../../assets/home_imj.jpg";
+import img from "../../../assets/icon.svg";
+import { CardAdv } from "../../../components/adv/CardAdvogados";
+import { getI18n } from "../../../utils/hooks/useGetI18n";
 
 export const HomePage = () => {
-	const navigate = useNavigate();
-	return (
-		<div>
-			<header className="flex h-5rem align-items-center justify-content-between">
-				<div className="flex">
-					<div className="flex align-items-center">
-						<img src={img} style={{ borderRadius: 5 }} className="mx-4" />
-					</div>
-					<div className="flex align-items-center">
-						<span>Encontrar Advogados</span>
-					</div>
-				</div>
-				<div>
-					<Button
-						outlined
-						className="mr-4"
-						label="login"
-						severity="secondary"
-						icon="pi pi-user"
-						onClick={() => {
-							navigate('/login');
-						}}
-						iconPos="right"
-					></Button>
-				</div>
-			</header>
-			<main
-				className="flex flex-column"
-				style={{
-					height: 'calc(200vh - 5rem)',
-					overflowY: 'auto',
-				}}
-			>
-				<div
-					className="flex"
-					style={{
-						height: '100vh',
-						backgroundColor: '#ffffff',
-						overflowY: 'auto',
-						// boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-					}}
-				>
-					<div className="flex flex-column-reverse md:flex-row w-screen align-items-center justify-content-evenly">
-						<div className="mx-4">
-							<div>
-								<h1>
-									Sua ferramenta para <br /> a busca de advogados
-								</h1>
-							</div>
-							<div>
-								<span>
-									Procurando um Advogado? <br /> O Advogapp te ajuda a encontrar
-									o advogado para o seu caso
-								</span>
-							</div>
-						</div>
-						<div className="mx-8">
-							<img
-								src={img_men}
-								style={{
-									borderRadius: 5,
-									width: '40vw',
-									boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-								}}
-								className="mx-4"
-							/>
-						</div>
-					</div>
-				</div>
-				<div
-					className="flex flex-column m-8"
-					style={{
-						height: '100vh',
-						overflowY: 'auto',
-						backgroundColor: '#ffffff',
-					}}
-				>
-					<Divider />
-					<CardAdv />
-				</div>
-			</main>
-		</div>
-	);
+  const navigate = useNavigate();
+  const homeI18n = getI18n("home");
+  return (
+    <div>
+      <header className="flex h-5rem align-items-center justify-content-between">
+        <div className="flex">
+          <div className="flex align-items-center">
+            <img src={img} style={{ borderRadius: 5 }} className="mx-4" />
+          </div>
+          <div className="flex align-items-center">
+            <span>{homeI18n.findLawyers}</span>
+          </div>
+        </div>
+        <div>
+          <Button
+            outlined
+            className="mr-4"
+            label={homeI18n.login}
+            severity="secondary"
+            icon="pi pi-user"
+            onClick={() => {
+              navigate("/login");
+            }}
+            iconPos="right"
+          ></Button>
+        </div>
+      </header>
+      <main
+        className="flex flex-column"
+        style={{
+          height: "calc(200vh - 5rem)",
+          overflowY: "auto",
+        }}
+      >
+        <div
+          className="flex"
+          style={{
+            height: "100vh",
+            backgroundColor: "#ffffff",
+            overflowY: "auto",
+            // boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <div className="flex flex-column-reverse md:flex-row w-screen align-items-center justify-content-evenly">
+            <div className="mx-4">
+              <div>
+                <h1>{homeI18n.yourToolForSearchingForLawyers}</h1>
+              </div>
+              <div>
+                <span>
+                  {homeI18n.lookingForALawyer} <br />
+                  {homeI18n.AdvogappHelpsYouFindTheLawyerForYourCase}
+                </span>
+              </div>
+            </div>
+            <div className="mx-8">
+              <img
+                src={img_men}
+                style={{
+                  borderRadius: 5,
+                  width: "40vw",
+                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                }}
+                className="mx-4"
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          className="flex flex-column m-8"
+          style={{
+            height: "100vh",
+            overflowY: "auto",
+            backgroundColor: "#ffffff",
+          }}
+        >
+          <Divider />
+          <CardAdv />
+        </div>
+      </main>
+    </div>
+  );
 };
--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,121 +1,121 @@
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { Password } from 'primereact/password';
-import { classNames } from 'primereact/utils';
-import { useEffect } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { Password } from "primereact/password";
+import { classNames } from "primereact/utils";
+import { useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 import {
-	showToastError,
-	showToastSuccess,
-} from '../../../components/GlobalToast';
-import { LogoTopbar } from '../../../components/LogoTopbar';
-import { api } from '../../../services/axios';
-import { getFormErrorMessage } from '../../../utils/hooks/useGetFormErrorMessage';
-import { getI18n } from '../../../utils/hooks/useGetI18n';
+  showToastError,
+  showToastSuccess,
+} from "../../../components/GlobalToast";
+import { LogoTopbar } from "../../../components/LogoTopbar";
+import { api } from "../../../services/axios";
+import { getFormErrorMessage } from "../../../utils/hooks/useGetFormErrorMessage";
+import { getI18n } from "../../../utils/hooks/useGetI18n";
 
 export const LoginPage = () => {
-	const loginI18n = getI18n('login');
-	const navigate = useNavigate();
-	const {
-		control,
-		formState: { errors },
-		handleSubmit,
-		watch,
-		register,
-	} = useForm();
+  const loginI18n = getI18n("login");
+  const navigate = useNavigate();
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    watch,
+    register,
+  } = useForm();
 
-	useEffect(() => {
-		watch('value');
-	}, [watch('value')]);
+  useEffect(() => {
+    watch("value");
+  }, [watch("value")]);
 
-	const onSubmit = (data: any) => {
-		api
-			.post('/auth/login', {
-				login: data.login,
-				password: data.password,
-			})
-			.then(() => {
-				navigate('/');
-				showToastSuccess('success');
-			})
-			.catch(() => {
-				showToastError('Usuário ou Senha Inválidos');
-			});
-	};
-	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<LogoTopbar />
-			<div className="h-screen w-screen flex justify-content-center align-items-center">
-				<div className="flex flex-column w-16rem">
-					<div className="text-center mb-2">
-						<h1>{loginI18n.title}</h1>
-					</div>
-					<div className="mb-2">
-						<InputText
-							className={classNames({
-								'p-invalid': errors.login,
-							})}
-							style={{ width: '100%' }}
-							placeholder={loginI18n.login}
-							id="login"
-							{...register('login', {
-								required: true,
-							})}
-						/>
-						{getFormErrorMessage(errors.login)}
-					</div>
-					<div className="mb-3">
-						<Controller
-							name="password"
-							control={control}
-							rules={{ required: true }}
-							render={({ field, fieldState }) => (
-								<>
-									<Password
-										onChange={(e) => field.onChange(e)}
-										placeholder={loginI18n.password}
-										className={classNames({
-											'p-invalid': fieldState.error,
-										})}
-										feedback={false}
-										toggleMask
-										inputStyle={{ width: '100%' }}
-									/>
-									{getFormErrorMessage(errors.password)}
-								</>
-							)}
-						/>
-					</div>
-					<div className="mb-3">
-						<Button className="w-full" label={loginI18n.login} />
-					</div>
-					<div className="text-center mb-3">
-						<div className="mb-1">
-							<span
-								onClick={() => {
-									navigate('/changepassword');
-								}}
-								className="no-underline hover:underline text-primary cursor-pointer"
-							>
-								{loginI18n.reset_password}
-							</span>
-						</div>
-						<div>
-							<span>{loginI18n.no_account}</span>
-							<span
-								onClick={() => {
-									navigate('/register');
-								}}
-								className="no-underline hover:underline text-primary cursor-pointer ml-2"
-							>
-								{loginI18n.register}
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</form>
-	);
+  const onSubmit = (data: any) => {
+    api
+      .post("/auth/login", {
+        login: data.login,
+        password: data.password,
+      })
+      .then(() => {
+        navigate("/");
+        showToastSuccess("success");
+      })
+      .catch(() => {
+        showToastError("Usuário ou Senha Inválidos");
+      });
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <LogoTopbar />
+      <div className="h-screen w-screen flex justify-content-center align-items-center">
+        <div className="flex flex-column w-16rem">
+          <div className="text-center mb-2">
+            <h1>{loginI18n.title}</h1>
+          </div>
+          <div className="mb-2">
+            <InputText
+              className={classNames({
+                "p-invalid": errors.login,
+              })}
+              style={{ width: "100%" }}
+              placeholder={loginI18n.login}
+              id="login"
+              {...register("login", {
+                required: true,
+              })}
+            />
+            {getFormErrorMessage(errors.login)}
+          </div>
+          <div className="mb-3">
+            <Controller
+              name="password"
+              control={control}
+              rules={{ required: true }}
+              render={({ field, fieldState }) => (
+                <>
+                  <Password
+                    onChange={(e) => field.onChange(e)}
+                    placeholder={loginI18n.password}
+                    className={classNames({
+                      "p-invalid": fieldState.error,
+                    })}
+                    feedback={false}
+                    toggleMask
+                    inputStyle={{ width: "100%" }}
+                  />
+                  {getFormErrorMessage(errors.password)}
+                </>
+              )}
+            />
+          </div>
+          <div className="mb-3">
+            <Button className="w-full" label={loginI18n.login} />
+          </div>
+          <div className="text-center mb-3">
+            <div className="mb-1">
+              <span
+                onClick={() => {
+                  navigate("/changepassword");
+                }}
+                className="no-underline hover:underline text-primary cursor-pointer"
+              >
+                {loginI18n.reset_password}
+              </span>
+            </div>
+            <div>
+              <span>{loginI18n.no_account}</span>
+              <span
+                onClick={() => {
+                  navigate("/register");
+                }}
+                className="no-underline hover:underline text-primary cursor-pointer ml-2"
+              >
+                {loginI18n.register}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
 };

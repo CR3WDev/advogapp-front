e9d970fdda6f04a39d124d201bb0d5f4b02ac669
Marcelo Távora <marcelovtneves@gmail.com>
2024-01-23 14:30:56 -0300

    ✨ Adicionado landing page

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,18 +1,24 @@
-import { Button } from 'primereact/button';
-import { useNavigate } from 'react-router-dom';
+import { LogoTopbar } from '@components/LogoTopbar'
+import { Button } from 'primereact/button'
+import { useNavigate } from 'react-router-dom'
 
 export const LandingPage = () => {
-	const navigate = useNavigate();
-
-	return (
-		<div>
-			teste de landing page
-			<Button
-				label="login"
-				onClick={() => {
-					navigate('/login');
-				}}
-			></Button>
-		</div>
-	);
-};
+  const navigate = useNavigate()
+  return (
+    <>
+      <LogoTopbar />
+      <div className="h-screen w-screen flex justify-content-center align-items-center">
+        <div className="flex flex-column w-16rem align-items-center">
+          <h1>Pagina Inicial</h1>
+          <Button
+            className="mt-3 w-full"
+            label={'login'}
+            onClick={() => {
+              navigate('/login')
+            }}
+          ></Button>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,36 +1,36 @@
-import { ChangePassowrdPage } from '@pages/Auth/ChangePassword';
-import { LoginPage } from '@pages/Auth/Login';
-import { RegisterPage } from '@pages/Auth/Register';
-import { LandingPage } from '@pages/LandingPage';
-import { NotFoundPage } from '@pages/NotFound';
-import { createBrowserRouter } from 'react-router-dom';
-import { AuthChecker } from './AuthChecker';
-import { PrivateRoutes } from './PrivateRoutes';
+import { ChangePassowrdPage } from '@pages/Auth/ChangePassword'
+import { LoginPage } from '@pages/Auth/Login'
+import { RegisterPage } from '@pages/Auth/Register'
+import { LandingPage } from '@pages/LandingPage'
+import { NotFoundPage } from '@pages/NotFound'
+import { createBrowserRouter } from 'react-router-dom'
+import { AuthChecker } from './AuthChecker'
+import { PrivateRoutes } from './PrivateRoutes'
 
 export const router = createBrowserRouter([
-	{
-		path: '/login',
-		element: <LoginPage />,
-	},
-	{
-		path: '/landingpage',
-		element: <LandingPage />,
-	},
-	{
-		path: '/register',
-		element: <RegisterPage />,
-	},
-	{
-		path: '/changepassword',
-		element: <ChangePassowrdPage />,
-	},
-	{
-		path: '/',
-		element: <AuthChecker />,
-		children: PrivateRoutes,
-	},
-	{
-		path: '*',
-		element: <NotFoundPage />,
-	},
-]);
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/landingpage',
+    element: <LandingPage />,
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />,
+  },
+  {
+    path: '/changepassword',
+    element: <ChangePassowrdPage />,
+  },
+  {
+    path: '/',
+    element: <AuthChecker />,
+    children: PrivateRoutes,
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
+  },
+])

86404280bdefc24b08bc1ac5ae121ec7bc9c912b
Saulomelo <saulo.melo@edu.unifor.br>
2024-02-13 17:40:46 -0300

    ✨ Regex na senha ao criar usuário

--- src/pages/Auth/Register/components/UserRegister/index.tsx
+++ src/pages/Auth/Register/components/UserRegister/index.tsx
@@ -1,7 +1,9 @@
 import { getFormErrorMessage } from '@utils/hooks/useGetFormErrorMessage'
 import { getI18n } from '@utils/hooks/useGetI18n'
 import { UseValidateEmail } from '@utils/hooks/useValidateEmail'
+import { UseValidatePassword } from '@utils/hooks/useValidatePassword'
 import { Button } from 'primereact/button'
+import { Divider } from 'primereact/divider'
 import { InputText } from 'primereact/inputtext'
 import { Password } from 'primereact/password'
 import { classNames } from 'primereact/utils'
@@ -14,6 +16,20 @@ export const UserRegister = () => {
   const registerI18n = getI18n('register')
   const { mutateAsync: userRegister } = postRegister()
   const navigate = useNavigate()
+
+  const passwordHeader = <div className="font-bold mb-3">{registerI18n.pick_a_password}</div>
+  const passwordFooter = (
+    <>
+      <Divider />
+      <p className="mt-2">Ao menos:</p>
+      <ul className="pl-2 ml-2 mt-0 line-height-3">
+        <li>{registerI18n.at_least_one_uppercase}</li>
+        <li>{registerI18n.at_least_one_numeric}</li>
+        <li>{registerI18n.at_least_one_special_characters}</li>
+        <li>{registerI18n.at_least_eight_characters}</li>
+      </ul>
+    </>
+  )
 
   const {
     control,
@@ -36,12 +52,12 @@ export const UserRegister = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="padding-responsiveness mb-2">
+      <div className="mb-2">
         <InputText
           className={classNames('w-full', {
             'p-invalid': errors.fullName,
           })}
-          placeholder={registerI18n.full_name + ' *'}
+          placeholder={registerI18n.full_name + '*'}
           id="fullName"
           {...register('fullName', {
             required: true,
@@ -49,12 +65,12 @@ export const UserRegister = () => {
         />
         {getFormErrorMessage(errors.fullName)}
       </div>
-      <div className="padding-responsiveness mb-2">
+      <div className="mb-2">
         <InputText
           className={classNames('w-full', {
             'p-invalid': errors.email,
           })}
-          placeholder={registerI18n.email + ' *'}
+          placeholder={registerI18n.email + '*'}
           id="email"
           {...register('email', {
             required: true,
@@ -65,27 +81,41 @@ export const UserRegister = () => {
         />
         {getFormErrorMessage(errors.email)}
       </div>
-      <div className="padding-responsiveness mb-2">
+      <div className="mb-2">
         <Controller
           name="password"
           control={control}
-          rules={{ required: true }}
+          rules={{
+            required: true,
+            validate: (e) => {
+              return UseValidatePassword(e) || 'Senha Inválida'
+            },
+          }}
           render={({ field, fieldState }) => (
-            <div className="custom-password">
+            <>
               <Password
                 onChange={(e) => field.onChange(e)}
-                placeholder={registerI18n.password}
                 className={classNames({ 'p-invalid': fieldState.error })}
-                feedback={false}
-                toggleMask
+                header={passwordHeader}
+                footer={passwordFooter}
+                placeholder={registerI18n.password + '*'}
+                strongRegex={'^(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&()_+])(?!.*\\s).{8,20}$'}
                 inputStyle={{ width: '100%' }}
+                maxLength={20}
+                toggleMask
+                promptLabel={registerI18n.choose_a_password}
+                weakLabel={registerI18n.too_weak}
+                mediumLabel={registerI18n.average}
+                strongLabel={registerI18n.strong_password}
+                id={field.name}
+                name={field.name}
               />
-            </div>
+              {getFormErrorMessage(errors.password)}
+            </>
           )}
         />
-        {getFormErrorMessage(errors.password)}
       </div>
-      <div className="padding-responsiveness mb-3">
+      <div className=" mb-3">
         <Controller
           name="confirmPassword"
           control={control}
@@ -99,7 +129,7 @@ export const UserRegister = () => {
             <div className="custom-password">
               <Password
                 onChange={(e) => field.onChange(e)}
-                placeholder={registerI18n.confirm_password}
+                placeholder={registerI18n.confirm_password + '*'}
                 className={classNames({ 'p-invalid': fieldState.error })}
                 feedback={false}
                 toggleMask
@@ -110,7 +140,7 @@ export const UserRegister = () => {
           )}
         />
       </div>
-      <div className="padding-responsiveness mb-3">
+      <div className=" mb-3">
         <Button className="w-full " label={registerI18n.create_account} />
       </div>
     </form>
